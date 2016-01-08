5aaab5d87a5221c77ddac1638a6e2a305bc3df19
Daniel <daniel.vernberg@gmail.com>
2016-01-08 23:58:54 +0100

    format in usage

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
       "/search/type/:contenttype/:search":            'search for something in a content group',
       "/search/type/:contenttype/:search/:page":      'search a specific page nr',
       "imdb/:imdbID":                                 'look up specific IMDb ID',
-      '/series/title/:series/:season':                      'list all episodes within a season. Uses OMDb API title (t=)',
+      '/series/title/:series/:season':                'list all episodes within a season. Uses OMDb API title (t=)',
       '/series/imdb/:imdbid/:season':                 'same as above only it uses IMDB ID instead of title (its more accurate)'
 
     }});
