b927bfc7075081e042144f2847ba325d55ee63be
Daniel <daniel.vernberg@gmail.com>
2016-01-09 14:16:39 +0100

    better indentation and require index routing

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 var express = require('express'),
     app = express();
-    
+
 // Require other modules (files)
-var router = require('./routes')(app);
-var config = require('./config')(app);
+var index =   require('./index')(app);
+var api =     require('./routes/api')(app);
+var user =    require('./routes/user')(app);
+var config =  require('./config')(app);
 
 // Port variable
 var port = 1337;
