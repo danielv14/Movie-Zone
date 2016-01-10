1a7f3156378e41fb92446d9e711501c61483722f
Daniel <daniel.vernberg@gmail.com>
2016-01-10 17:31:01 +0100

    added a test controller for fetching frozen

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,15 +1,26 @@
 angular
   .module('app')
+
   .controller('contactCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.greeting = "HOLA";
     $http({
       method: 'GET',
       url: 'http://localhost:1337/api'
     }).then(function successCallback(response) {
-      console.log('request worked');
       console.log(response);
     }), function errorCallback(response) {
-      console.log('error');
+      console.log(response);
+    }
+  }])
+
+  // Test controller
+  .controller('testCtrl', ['$scope', '$http', function($scope, $http) {
+    $http({
+      method: 'GET',
+      url: 'http://www.omdbapi.com/?i=tt2294629'
+    }).then(function successCallback(response) {
+      $scope.details = response.data;
+    }), function errorCallback(response) {
       console.log(response);
     }
   }])
