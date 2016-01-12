a60e54036b8a47d313e6220899676ebeb215a08f
Daniel <daniel.vernberg@gmail.com>
2016-01-12 10:21:25 +0100

    test route to create a table

--- server/routes/watchlist.js
+++ server/routes/watchlist.js
@@ -28,6 +28,14 @@ module.exports = function(app) {
 
   });
 
+  router.get('/test', function (req, res) {
+
+    r.db('moviezone').tableCreate('authors').run(connection, function(err, result) {
+      if (err) throw err;
+      console.log(JSON.stringify(result, null, 2));
+    })
+
+  });
 
 
 
