ffc1b1abab3b3c1da6dfdf2bb5666affe38742ad
Daniel <daniel.vernberg@gmail.com>
2016-01-12 12:26:29 +0100

    list all movies

--- server/routes/watchlist.js
+++ server/routes/watchlist.js
@@ -88,7 +88,18 @@ module.exports = function(app) {
   // get just the movies
   router.get('/find/movie', function (req, res) {
 
-  })
+    r.db('moviezone').table('watchlist').filter(r.row('type').eq("movie")).run(connection, function(err, cursor) {
+      if (err) throw err;
+      cursor.toArray(function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+        res.send(result);
+
+      });
+    });
+  }) // end of route
+
+
 
   // get just the series
   router.get('/find/series', function (req, res) {
