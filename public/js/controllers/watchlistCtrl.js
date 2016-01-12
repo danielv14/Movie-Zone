bc0d59f8054b2b07eb366ad8a364aa811d419b73
Daniel <daniel.vernberg@gmail.com>
2016-01-12 12:43:38 +0100

    simple controller in place

--- public/js/controllers/watchlistCtrl.js
+++ public/js/controllers/watchlistCtrl.js
@@ -5,4 +5,13 @@ angular
 
   .controller('watchlistCtrl', ['$scope', '$http', function($scope, $http) {
     console.log('controller working');
+    $http({
+      method: 'GET',
+      url: 'http://localhost:1337/watchlist/find/all'
+    }).then(function successCallback(response) {
+      console.log(response.data);
+      $scope.watchlist = response.data;
+    }), function errorCallback(response) {
+      console.log(response.data);
+    }
   }])
